a3c5a0587f79b15f3d71ae3a2e4bafc7418fb451
mlt233 <yuwu2@student.unimelb.edu.au>
2022-12-13 17:27:40 +0800

    update

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-import './style.css'
-import * as THREE from 'three';
+import "./style.css";
+import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(
+	75,
+	window.innerWidth / window.innerHeight,
+	0.1,
+	1000
+);
 
 const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector("#bg"),
+	canvas: document.querySelector("#bg"),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -14,15 +19,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.z = 30;
 
-const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
-const material = new THREE.MeshBasicMaterial({color: ff99cc, wireframe: true});
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshBasicMaterial({
+	color: new THREE.Color("#ff99ca"),
+	wireframe: true,
+});
 const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
-function animate(){
-  requestAnimationFrame(animate);
-  renderer.render(scene, camera);
+function animate() {
+	requestAnimationFrame(animate);
+	renderer.render(scene, camera);
 }
 
 animate();
